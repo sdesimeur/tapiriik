b43eb407bdc5e907cdd894fb86c5e31f8e35355b
sam <sam@sdesimeur.com>
2020-12-02 21:39:42 +0000

    nothing

--- runAll.sh
+++ runAll.sh
@@ -2,5 +2,7 @@
 cd /divers/tapiriik
 source tap-env/bin/activate
 /usr/bin/python3 manage.py runserver >/var/log/tapiriik/server.log 2>/var/log/tapiriik/server_error.log &
+sleep 10s
 /usr/bin/python3 ./sync_worker.py >/var/log/tapiriik/sync_worker.log 2>/var/log/tapiriik/sync_worker_error.log &
+sleep 10s
 /usr/bin/python3 ./sync_scheduler.py >/var/log/tapiriik/sync_scheduler.log 2>/var/log/tapiriik/sync_scheduler_error.log &
